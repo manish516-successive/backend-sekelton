eddaffc2001caf381cf25f66ada6b2f7a901d75f
Manish Agarwal <manish.agarwal@manishr301.successivetech.com>
2021-07-01 16:32:34 +0530

    [MA] A generic solution for logging at different levels (needs some investigation) with Correlation ID, time spent at individual calls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
+import { LoggingModule } from "./logging/logging.module"
+
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [EmployeeModule, LoggingModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule  {}
---
+++ src/logging/logging.service.ts
@@ -0,0 +1,23 @@
+import { LoggerService } from '@nestjs/common';
+import { getValue } from "express-ctx";
+import { LOGGER_KEY } from "./constants";
+
+
+export class LoggingService implements LoggerService {
+  log(message: string) {
+    getValue(LOGGER_KEY).info(message);
+  }
+  error(message: string, trace: string) {
+    getValue(LOGGER_KEY).error(message, trace);
+  }
+  warn(message: string) {
+   getValue(LOGGER_KEY).warn(message);
+  }
+  debug(message: string) {
+    getValue(LOGGER_KEY).debug(message);
+  }
+  verbose(message: string) {
+   getValue(LOGGER_KEY).verbose(message);
+  }
+}
+
